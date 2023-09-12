00cdcdeb3790bc84ce0243e454fca547933ed5e3
MaxyMarquez <maxy.marquez@hotmail.com>
2023-09-12 18:07:42 -0300

    cambios

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <section className='flex items-center justify-center w-full h-screen bg-transparent'>
+
+        </section>
+    )
+}
+
+export default Projects
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useParallax, Parallax } from 'react-scroll-parallax';
 import htmlLogo from '../../assets/html-5.png';
 import cssLogo from '../../assets/css-3.png';
 import reactLogo from '../../assets/react.png';
@@ -87,42 +88,80 @@ const Skills = () => {
     };
 
     return (
-        <section id='about' className='flex items-center justify-center max-lg:flex-col w-full h-screen relative font-mono'>
+        <section id='about' className='flex flex-col items-center justify-center max-lg:flex-col w-full h-auto py-10 relative font-mono overflow-hidden'>
             <div className='flex flex-col items-center justify-center w-full'>
-                <h1 className='text-white text-6xl text-shadow mx-5'>About me</h1>
+                <h1 className='text-gray-600 text-6xl text-shadow mx-5'>About me</h1>
                 <p className='text-center px-10 mt-5 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta officiis, ratione laudantium nesciunt error minus. Vitae, aperiam nam voluptatum molestias quas, vero a debitis sit nostrum, eum ut officia error!</p>
 
             </div>
-            <div className='skills-container flex flex-col items-center justify-center w-full h-full '>
-                <h1 className='text-white text-6xl text-shadow mx-5'>Skills</h1>
-                <div className='relative flex items-center justify-center duration-500 mt-5'>
-                    <button
-                        className={`text-white text-4xl text-shadow border max-md:w-40 max-md:text-2xl w-52 py-3 z-50 cursor-pointer ${isActive ? 'bg-red-600' : ''}`}
-                        onClick={handleFrontEndClick}
-                        disabled={isActive}
-                    >
-                        Front End
-                    </button>
-                    <button
-                        className={`text-white text-4xl text-shadow border max-md:w-40 max-md:text-2xl w-52 py-3 z-50 cursor-pointer ${!isActive ? 'bg-red-600' : ''}`}
-                        onClick={handleBackEndClick}
-                        disabled={!isActive}
-                    >
-                        Back End
-                    </button>
-                </div>
-                <div className={` ${animation ? 'fade-in' : 'fade-out'} bg-transparent w-full flex justify-center items-center flex-wrap flex-col`}>
-                    <ul className='w-96 max-sm:w-72 mt-5 z-50 bg-transparent grid grid-cols-3 gap-x-5 gap-y-2 grid-rows-2 place-items-center'>
-                        {activeSkills.map((skill, index) => (
-                            <li key={index} className='group sm:w-32 w-24 sm:h-32 h-24 bg-neutral-900 hover:bg-neutral-950  duration-300  p-5 relative flex items-center justify-center'>
-                                <img className='group-hover:scale-110 bg-transparent p-3 duration-300' src={skill.image} alt='' />
-                                <p className='absolute bottom-0 text-gray-500 bg-transparent'>{skill.name}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+            <div className='skills-container flex flex-col items-center justify-center w-full h-screen bg-transparent'>
+                <Parallax
+                    translateY={['0%', '100%']}
+                    scale={[.5, 1]}
+                    easing="easeInQuad"
+
+
+                >
+                    <div className=''>
+                        <h1 className='max-sm:text-6xl text-9xl text-gray-600 text-shadow'>Frontend</h1>
+
+                    </div>
+                </Parallax>
+                <Parallax
+                    translateX={['-10%', '50%']}
+                    scale={[1, 1]}
+                    easing="easeInQuad"
+                    speed={5}
+                    style={{ background: 'transparent' }}
+
+                >
+                    <div className='bg-transparent w-full flex justify-center items-center flex-wrap'>
+                        <ul className='mt-5 z-50 bg-transparent flex justify-start'>
+                            {frontEndSkills.map((skill, index) => (
+                                <li key={index} className='bg-transparent bg-neutral-900  duration-300  p-5 relative flex items-center justify-center'>
+                                    <img className='bg-transparent max-sm:p-0 p-3 w-[350px]' src={skill.image} alt='' />
+                                    <p className='absolute bottom-0 text-gray-500 max-sm:text-[15px] bg-transparent'>{skill.name}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Parallax>
             </div>
-        </section>
+            <div className='skills-container flex flex-col items-center justify-center w-full h-screen py-10 mt-10 bg-transparent'>
+                <Parallax
+                    translateY={['-10%', '100%']}
+                    scale={[.5, 1]}
+                    easing="easeInQuad"
+
+
+                >
+                    <div className=''>
+                        <h1 className='text-9xl text-gray-600 text-shadow'>Backend</h1>
+
+                    </div>
+                </Parallax>
+                <Parallax
+                    translateX={['50%', '-40%']}
+                    scale={[1, 1]}
+                    easing="easeOutQuad"
+                    speed={5}
+                    style={{ background: 'transparent' }}
+
+                >
+                    <div className='bg-transparent w-full flex justify-center items-center flex-wrap'>
+                        <ul className='w-50 mt-5 z-50 bg-transparent flex justify-start'>
+                            {backEndSkills.map((skill, index) => (
+                                <li key={index} className='bg-transparent bg-neutral-900 p-5 relative flex items-center justify-center'>
+                                    <img className='bg-transparent p-3 w-[200px]' src={skill.image} alt='' />
+                                    <p className='absolute bottom-0 text-gray-500 bg-transparent'>{skill.name}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Parallax>
+            </div>
+
+        </section >
     );
 };
 
